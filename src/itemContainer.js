8cc478ea62e50364d1c233d6f045c9582cf9ef43
yush-nh <yushin.nihei@luxiar.com>
2024-11-27 14:35:12 +0900

    Disable light dismiss function of popover attribute

--- src/itemContainer.js
+++ src/itemContainer.js
@@ -10,7 +10,7 @@ export default class ItemContainer {
     this.#inputElement = inputElement
     this.#onRender = onRender
     this.#container = document.createElement('ul')
-    this.#container.setAttribute('popover', 'auto')
+    this.#container.setAttribute('popover', 'manual')
     this.#container.classList.add('autocomplete')
     this.#container.style.margin = 0 // Clear popover default style.
     inputElement.parentElement.appendChild(this.#container)
